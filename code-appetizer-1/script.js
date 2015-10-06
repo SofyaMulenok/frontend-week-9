7bf702fb29d3acfbb23558d2805b7e3b59c8500f
Hackbright Student <info@hackbrightacademy.com>
2015-10-05 20:19:04 -0700

    update

--- code-appetizer-1/script.js
+++ code-appetizer-1/script.js
@@ -8,6 +8,18 @@ $(document).ready(function(){
   //  2. convert the email to all lowercase
   //  3. create the MD5 hash of that string using the MD5 function provided
   // MD5("stuff") will return the MD5 hash of the string "stuff"
+$("#email").keyup( function() {
 
+var email = $("#email").val();
+result = email.trim();
+final_result = result.toLowerCase();
+console.log(final_result);
+MD5result = MD5(final_result);
+
+console.log(MD5result);
+
+$('#avatar').attr('src',"http://www.gravatar.com/avatar/"+MD5result+"?s=256");
+
+});
 
 });
